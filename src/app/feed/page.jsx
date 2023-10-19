5ac698447d37ab966aa24d3abeb0a817bb55e1a3
Md. Ashiqul Islam <ashiqul.ba@pakiza.com>
2023-10-20 00:19:33 +0600

    updated

--- src/app/feed/page.jsx
+++ src/app/feed/page.jsx
@@ -1,9 +1,61 @@
+"use client"
 import React from 'react'
+import { Button, Stack } from 'react-bootstrap'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const page = () => {
+
+
+const feed = () => {
+    const [launches, setLaunches] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 9;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://api.spacexdata.com/v3/launches');
+        setLaunches(response.data);
+      } catch (error) {
+        console.error('Error fetching data', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentLaunches = launches.slice(indexOfFirstItem, indexOfLastItem);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+  
   return (
-    <div>page</div>
-  )
-}
+    <div>
+    <div>
+      {currentLaunches.map((launch) => (
+        <div key={launch.flight_number}>
+          <div>{launch.rocket.rocket_name}</div>
+          <div>{launch.launch_date_utc}</div>
+          {/* Add more data fields as needed */}
+        </div>
+      ))}
+    </div>
 
-export default page
+    {/* Pagination controls */}
+    <div>
+      {Array.from({ length: Math.ceil(launches.length / itemsPerPage) }).map((_, index) => (
+        <button key={index} onClick={() => paginate(index + 1)}>
+          {index + 1}
+        </button>
+      ))}
+    </div>
+  </div>
+);
+}
+  
+
+
+export default feed
