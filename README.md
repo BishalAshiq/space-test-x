189e4fdec5f9f1db5c68e7f2739bb8e5361eb5ea
Md. Ashiqul Islam <ashiqul.ba@pakiza.com>
2023-10-21 20:14:01 +0600

    almost done

--- README.md
+++ README.md
@@ -1,36 +1,34 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
-## Getting Started
+#### This is a Space-X-test project. Created by Next.js 
 
-First, run the development server:
+This is live server (https://test-space-x.netlify.app)
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+Created a React leatest which is next.js Project and using every component by JSX.
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+Used Context, hooks (axios), routing by next.js pattern. and all the functional component with no props drilling.
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+Used Given API
 
-## Learn More
+Build the given Figma interface. 
 
-To learn more about Next.js, take a look at the following resources:
+Used External framework Bootstrap 5
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+Used customize css variables to change the typo and color and alignments.
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+Made the page responsive with extra two displa, Tab + mobile.
 
-## Deploy on Vercel
+Data can be show by search by it's rocket name
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+It can show launch status ( Failure, Success )
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+Launch date also data can get on consol. 
+
+All upcoming data also show
+
+Added paginations, Only 9 itmes show each page.
+
+
+
+
+
--- src/app/feed/page.jsx
+++ src/app/feed/page.jsx
@@ -1,6 +1,6 @@
 "use client"
-import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 
@@ -8,21 +8,21 @@ const Feed = () => {
   const [launches, setLaunches] = useState([]);
   const [rocketNameFilter, setRocketNameFilter] = useState('');
   const [searchFilter, setSearchFilter] = useState('');
+  
   const [launchDateFilter, setLaunchDateFilter] = useState('By Launch Date');
-  const [lastYearSelected, setLastYearSelected] = useState(false);
-  const [lastMonthSelected, setLastMonthSelected] = useState(false);
-  const [lastWeekSelected, setLastWeekSelected] = useState(false);
-
+  const [showUpcoming, setShowUpcoming] = useState(false); 
+  const [filter, setFilter] = useState('All');
   const itemsPerPage = 9;
   const [currentPage, setCurrentPage] = useState(1);
-
+  
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://api.spacexdata.com/v3/launches');
+        const response = await axios
+        .get('https://api.spacexdata.com/v3/launches');
         setLaunches(response.data);
       } catch (error) {
-        console.error('Error  data', error);
+        console.error('Error fetching data', error);
       }
     };
 
@@ -37,39 +37,59 @@ const Feed = () => {
   const filterLaunches = () => {
     let filteredLaunches = [...launches];
 
-    // Filter by Rocket Name
-    if (rocketNameFilter) {
-      filteredLaunches = filteredLaunches.filter((launch) =>
-        launch.rocket.rocket_name.toLowerCase().includes(rocketNameFilter.toLowerCase())
-      );
-    }
+  
+  
     if (searchFilter) {
       filteredLaunches = filteredLaunches.filter((launch) =>
         launch.mission_name.toLowerCase().includes(searchFilter.toLowerCase())
       );
     }
 
-    if (lastYearSelected) {
+    if (rocketNameFilter) {
+      filteredLaunches = filteredLaunches.filter((launch) =>
+        launch.rocket.rocket_name.toLowerCase().includes(rocketNameFilter.toLowerCase())
+      );
+    }
+
+    // Filter by Launch Date
+
+    const today = new Date();
+    if (launchDateFilter === 'Last Year') {
+    
       const lastYear = new Date();
       lastYear.setFullYear(lastYear.getFullYear() - 1);
       filteredLaunches = filteredLaunches.filter((launch) =>
-        new Date(launch.launch_date_utc) > lastYear
+        new Date(launch.launch_year) > lastYear
       );
-    } else if (lastMonthSelected) {
+    } else if (launchDateFilter === 'Last Month') {
+    
       const lastMonth = new Date();
       lastMonth.setMonth(lastMonth.getMonth() - 1);
       filteredLaunches = filteredLaunches.filter((launch) =>
         new Date(launch.launch_date_utc) > lastMonth
       );
-    } else if (lastWeekSelected) {
+    } else if (launchDateFilter === 'Last Week') {
+  
       const lastWeek = new Date();
       lastWeek.setDate(lastWeek.getDate() - 7);
       filteredLaunches = filteredLaunches.filter((launch) =>
-        new Date(launch.launch_date_utc) > lastWeek
+        new Date(launch.launch_date_local) > lastWeek
+      );
+      
+    }
+    console.log("Filtered Launches (after date filter):", filteredLaunches);
+
+    if (showUpcoming) {
+      const today = new Date();
+      filteredLaunches = filteredLaunches.filter((launch) =>
+        new Date(launch.launch_date_utc) > today
       );
     }
 
     return filteredLaunches;
+  };
+  const handleRocketNameChange = (event) => {
+    setRocketNameFilter(event.target.value);
   };
 
   const handleSearchChange = (event) => {
@@ -77,14 +97,28 @@ const Feed = () => {
   };
 
   const handleLaunchDateChange = (event) => {
-    const selectedValue = event.target.value;
-    setLaunchDateFilter(selectedValue);
-
-    // Set the state variables for the selected interval
-    setLastYearSelected(selectedValue === 'Last Year');
-    setLastMonthSelected(selectedValue === 'Last Month');
-    setLastWeekSelected(selectedValue === 'Last Week');
+    setLaunchDateFilter(event.target.value);
   };
+
+  const handleShowUpcomingChange = () => {
+    setShowUpcoming(!showUpcoming);
+  };
+
+
+  const handleWeekChange = (event) => {
+    setWeekFilter(event.target.value);
+  };
+
+  const handleMonthChange = (event) => {
+    setMonthFilter(event.target.value);
+  };
+
+  const handleYearChange = (event) => {
+    setYearFilter(event.target.value);
+  };
+
+
+
 
   // Pagination function
   const paginate = (pageNumber) => {
@@ -109,7 +143,13 @@ const Feed = () => {
         </div>
 
         <div className="check-input-div">
-          <input type="checkbox" id="scales" name="scales" checked />
+          <input
+            type="checkbox"
+            id="scales"
+            name="scales"
+            checked={showUpcoming}
+            onChange={handleShowUpcomingChange}
+          />
           <span>Show upcoming only</span>
         </div>
 
@@ -121,46 +161,60 @@ const Feed = () => {
                   className='search-input'
                     type="search"
                     placeholder="Search.."
-                    value={searchFilter}
-                    onChange={handleSearchChange}
+                    value={rocketNameFilter}
+                    onChange={handleRocketNameChange}
                   />
                   <svg xmlns="http://www.w3.org/2000/svg" width="0" height="0" fill="white" className="bi bi-search search-icon" viewBox="0 0 16 16">
                     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
                   </svg>
               </div>
+              <div className=" mobile">
+          <input
+            type="checkbox"
+            id="scales"
+            name="scales"
+            checked={showUpcoming}
+            onClick={handleShowUpcomingChange}
+          />
+          <span>Show upcoming only</span>
+        </div>
             </Col>
 
 
             <Col xs={12} md={6} lg={6}>
               <Row>
 
-                <Col xs={12} md={6} lg={6}>
-                  <Form.Select
-                    size="sm"
-                  >
+                <Col xs={12} md={6} lg={6} className='launch-state-by'>
+                  
+
+                  <select class="form-select" aria-label="Default select example"  >
+                  
                     <option>By Launch Status</option>
-                  </Form.Select>
+                  </select>
                 </Col>
 
 
                 <Col xs={12} md={6} lg={6}>
-                  <Form.Select
-                    size="sm"
-                    onChange={handleLaunchDateChange}
-                    value={launchDateFilter}
-                  >
-                    <option>By Launch Date</option>
-                    <option>Last Week</option>
-                    <option>Last Month</option>
-                    <option>Last Year</option>
-                  </Form.Select>
+             
+                  <select 
+                  className="form-select" 
+                  aria-label="Default select example"   
+                  onChange={handleLaunchDateChange}
+                  value={launchDateFilter}>
+
+                    <option selected>By Launch Date</option>
+                    <option value="Last Week">Last Week</option>
+                    <option value="Last Month">Last Month</option>
+                    <option value="Last Year">Last Year</option>
+
+                  </select>
                 </Col>
               </Row>
             </Col>
           </Row>
         </div>
 
-        <div>
+        <div className='data-div'>
           <Row>
             {currentLaunches.map((launch) => (
               <Col xs={12} md={6} lg={4} key={launch.flight_number}>
@@ -199,191 +253,4 @@ const Feed = () => {
 };
 
 export default Feed;
-
-
-// import React from 'react'
-// import { useState, useEffect } from 'react';
-// import axios from 'axios';
-// import { Col, Container, Row } from 'react-bootstrap';
-// import Form from 'react-bootstrap/Form';
-// import Image from 'next/image';
-
-
-
-// const feed = () => {
-//   const [launches, setLaunches] = useState([]);
-//   const [rocketNameFilter, setRocketNameFilter] = useState('');
-//   const [launchDateFilter, setLaunchDateFilter] = useState('');
-
-//   const [searchFilter, setSearchFilter] = useState(''); 
-
-//   const [lastYearSelected, setLastYearSelected] = useState(false);
-//   const [lastMonthSelected, setLastMonthSelected] = useState(false);
-//   const [lastWeekSelected, setLastWeekSelected] = useState(false);
-  
-//   const itemsPerPage = 9;
-//   const [currentPage, setCurrentPage] = useState(1);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await axios.get('https://api.spacexdata.com/v3/launches');
-//         setLaunches(response.data);
-//       } catch (error) {
-//         console.error('Error fetching data', error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   const formatDate = (dateString) => {
-//     const options = { day: 'numeric', month: 'short' };
-//     return new Date(dateString).toLocaleDateString(undefined, options);
-//   };
-
-//   const filterLaunches = () => {
-//     let filteredLaunches = [...launches];
-
-//     // Filter by Rocket Name
-//     if (rocketNameFilter) {
-//       filteredLaunches = filteredLaunches.filter((launch) =>
-//         launch.rocket.rocket_name.toLowerCase().includes(rocketNameFilter.toLowerCase())
-//       );
-//     }
-//     if (searchFilter) {
-//       filteredLaunches = filteredLaunches.filter((launch) =>
-//         launch.mission_name.toLowerCase().includes(searchFilter.toLowerCase())
-//       );
-//     }
-
-//   // Filter by Launch Date
-//   if (lastYearSelected) {
-//     // Filter launches from the last year
-//     const lastYear = new Date();
-//     lastYear.setFullYear(lastYear.getFullYear() - 1);
-//     filteredLaunches = filteredLaunches.filter((launch) =>
-//       new Date(launch.launch_date_utc) > lastYear
-//     );
-//   } else if (lastMonthSelected) {
-//     // Filter launches from the last month
-//     const lastMonth = new Date();
-//     lastMonth.setMonth(lastMonth.getMonth() - 1);
-//     filteredLaunches = filteredLaunches.filter((launch) =>
-//       new Date(launch.launch_date_utc) > lastMonth
-//     );
-//   } else if (lastWeekSelected) {
-//     // Filter launches from the last week
-//     const lastWeek = new Date();
-//     lastWeek.setDate(lastWeek.getDate() - 7);
-//     filteredLaunches = filteredLaunches.filter((launch) =>
-//       new Date(launch.launch_date_utc) > lastWeek
-//     );
-//   }
-
-//   return filteredLaunches;
-//   };
-
-//   const handleSearchChange = (event) => {
-//     setSearchFilter(event.target.value);
-//   };
-
-//   const handleLaunchDateChange = (event) => {
-//     const selectedValue = event.target.value;
-//     setLaunchDateFilter(selectedValue);
-    
-//     // Set the state variables for the selected interval
-//     setLastYearSelected(selectedValue === 'Last Year');
-//     setLastMonthSelected(selectedValue === 'Last Month');
-//     setLastWeekSelected(selectedValue === 'Last Week');
-//   };
-
-//     // Pagination function
-//     const paginate = (pageNumber) => {
-//       setCurrentPage(pageNumber);
-//     };
-  
-//     const indexOfLastItem = currentPage * itemsPerPage;
-//     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-//     const currentLaunches = filterLaunches().slice(indexOfFirstItem, indexOfLastItem);
-  
-//   return (
-//     <div>
-//       <Container>
-//           <div className='header-tag-div'>
-//                 <h2>Spaceflight details</h2>
-//                 <p>Find out the elaborate features of all past big spaceflights</p>
-//           </div>
-
-//           <div className='check-input-div'>
-//        <input type="checkbox" id="scales" name="scales"
-//          checked/>
-//          <span>Show upcomming only</span>
-//        </div>
-
-//        <div className="header-imputs-div">
-//         <Row>
-//           <Col xs={12} md={6} lg={6}>
-//             <input
-//               type="search"
-//               placeholder="Search by Rocket Name"
-//               value={searchFilter}
-//                 onChange={handleSearchChange}
-//             />
-//           </Col>
-//           <Col xs={12} md={6} lg={6}>
-//             <Row>
-//               <Col xs={12} md={6} lg={6}>
-//                 <Form.Select size="sm">
-//                   <option>By Launch Date</option>
-//                   <option>Last Week</option>
-//                   <option>Last Month</option>
-//                   <option>Last Year</option>
-//                 </Form.Select>
-//               </Col>
-//               <Col xs={12} md={6} lg={6}>
-//                 <Form.Select size="sm"     onChange={handleLaunchDateChange}
-//                     value={launchDateFilter}>
-//                   <option>By Launch Date</option>
-//                   <option>Last Week</option>
-//                   <option>Last Month</option>
-//                   <option>Last Year</option>
-//                 </Form.Select>
-//               </Col>
-//             </Row>
-//           </Col>
-//         </Row>
-//       </div>
-   
-   
-//     <div>
-    
-//     <Row>
-//         {filterLaunches().map((launch) => (
-//           <Col xs={12} md={6} lg={4} key={launch.flight_number}>
-//             <img src={launch.links.mission_patch_small} width={100} height={100} alt="" />
-//             <div>Date: {formatDate(launch.launch_date_local)}, <span>{launch.launch_year}</span></div>
-//             <div>{launch.mission_name}</div>
-//             <div>{launch.rocket.rocket_name}</div>
-//           </Col>
-//         ))}
-//       </Row>
-     
-//     </div>
-
-//     <div>
-//       {Array.from({ length: Math.ceil(launches.length / itemsPerPage) }).map((_, index) => (
-//         <button key={index} onClick={() => paginate(index + 1)}>
-//           {index + 1}
-//         </button>
-//       ))}
-//     </div>
-//     </Container>
-//   </div>
-// );
-// }
-  
-
-
-// export default feed
 
